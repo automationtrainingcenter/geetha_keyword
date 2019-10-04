5311df177583f14fd5c954f170a4d1b56c536f6f
automation <atcsurya@gmail.com>
2019-10-04 08:54:20 +0530

    added reports and final

--- src/test/java/banking/keyworddriven/Driver.java
+++ src/test/java/banking/keyworddriven/Driver.java
@@ -5,12 +5,18 @@ import java.lang.reflect.Method;
 
 import org.junit.Test;
 
-public class Driver {
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.LogStatus;
+
+import utilities.GenericHelper;
+
+public class Driver extends GenericHelper{
 
 	@Test
 	public void execute() {
 		Keywords keywords = new Keywords();
 		Method[] methods = keywords.getClass().getMethods();
+		keywords.report = new ExtentReports(getPath("reports", "report.html"));
 		// create ExcelHelper class objects so that we can read data from the excelfiles
 		ExcelHelper tcExcel = new ExcelHelper();
 		tcExcel.openExcel("resources", "keywords.xlsx", "testcases");
@@ -18,37 +24,53 @@ public class Driver {
 		tsExcel.openExcel("resources", "keywords.xlsx", "teststeps");
 
 		// iterate over rows inside the test case excel
-		for (int i = 1; i <= tcExcel.getRows(); i++) {
-			// retrieve the data from test case excel
-			String tcname = tcExcel.readData(i, 1);
-			String runMode = tcExcel.readData(i, 2);
-			if (runMode.equals("yes")) {
-				// iterate over rows inside the test steps excel
-				for (int j = 1; j <= tsExcel.getRows(); j++) {
-					// retrieve the data from test step excel
-					String tsd_tcname = tsExcel.readData(j, ExcelColumns.TSD_TCNAME);
-					if (tcname.equals(tsd_tcname)) {
-						String stepName = tsExcel.readData(j, ExcelColumns.STEPNAME);
-						String locType = tsExcel.readData(j, ExcelColumns.LOCTYPE);
-						String locValue = tsExcel.readData(j, ExcelColumns.LOCVALUE);
-						String action = tsExcel.readData(j, ExcelColumns.ACTION);
-						String testData = tsExcel.readData(j, ExcelColumns.TESTDATA);
-						for(Method method : methods) {
-							if(method.getName().equals(action)) {
-								try {
-									method.invoke(keywords, locType, locValue, testData);
-									
-									break;
-								} catch (IllegalAccessException | IllegalArgumentException
-										| InvocationTargetException e) {
-									// TODO Auto-generated catch block
-									e.printStackTrace();
+		try {
+			for (int i = 1; i <= tcExcel.getRows(); i++) {
+				// retrieve the data from test case excel
+				String tcname = tcExcel.readData(i, 1);
+				String runMode = tcExcel.readData(i, 2);
+				if (runMode.equals("yes")) {
+					keywords.test = keywords.report.startTest(tcname);
+					// iterate over rows inside the test steps excel
+					try {
+						for (int j = 1; j <= tsExcel.getRows(); j++) {
+							// retrieve the data from test step excel
+							String tsd_tcname = tsExcel.readData(j, ExcelColumns.TSD_TCNAME);
+							if (tcname.equals(tsd_tcname)) {
+								String stepName = tsExcel.readData(j, ExcelColumns.STEPNAME);
+								String locType = tsExcel.readData(j, ExcelColumns.LOCTYPE);
+								String locValue = tsExcel.readData(j, ExcelColumns.LOCVALUE);
+								String action = tsExcel.readData(j, ExcelColumns.ACTION);
+								String testData = tsExcel.readData(j, ExcelColumns.TESTDATA);
+								keywords.test.log(LogStatus.INFO, "executing "+stepName);
+								for(Method method : methods) {
+									if(method.getName().equals(action)) {
+										try {
+											method.invoke(keywords, locType, locValue, testData);
+											
+											break;
+										} catch (IllegalAccessException | IllegalArgumentException
+												| InvocationTargetException e) {
+											// TODO Auto-generated catch block
+											e.printStackTrace();
+										}
+									}
 								}
 							}
 						}
+						keywords.report.endTest(keywords.test);
+					} catch (Exception e) {
+						keywords.test.log(LogStatus.INFO, tcname+ " failed");
 					}
 				}
 			}
+		} catch (Exception e) {
+			
 		}
+		finally {
+			keywords.report.flush();
+			keywords.report.close();
+		}
+		
 	}
 }
--- src/test/java/banking/keyworddriven/Keywords.java
+++ src/test/java/banking/keyworddriven/Keywords.java
@@ -13,46 +13,61 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 import utilities.BrowserHelper;
 
 public class Keywords extends BrowserHelper {
 	WebDriver driver;
-
+	ExtentReports report;
+	ExtentTest test;
 //	openBrowser
 	public void openBrowser(String locType, String locValue, String testdata) {
 		driver = openBrowser(testdata);
+		test.log(LogStatus.INFO, "browser launched successfully");
 	}
 
 //	navigate
 	public void navigate(String locType, String locValue, String testdata) {
 		this.driver.get(testdata);
+		test.log(LogStatus.INFO, "navigate to "+testdata);
 	}
 
 //	setText
 	public void setText(String locType, String locValue, String testdata) {
+		test.log(LogStatus.INFO, "locating an element using "+locType+" = "+locValue);
 		this.driver.findElement(LocatorHelper.locateElement(locType, locValue)).sendKeys(testdata);
+		test.log(LogStatus.INFO, "element located and entered "+testdata);
 		sleep(2000);
 	}
 
 //	click
 	public void click(String locType, String locValue, String testdata) {
+		test.log(LogStatus.INFO, "locating an element using "+locType+" = "+locValue);
 		this.driver.findElement(LocatorHelper.locateElement(locType, locValue)).click();
+		test.log(LogStatus.INFO, "clicking on element");
 		sleep(2000);
 	}
 
 //	moveToElement
 	public void moveToElement(String locType, String locValue, String testdata) {
 		Actions actions = new Actions(this.driver);
+		test.log(LogStatus.INFO, "locating an element using "+locType+" = "+locValue);
 		actions.moveToElement(this.driver.findElement(LocatorHelper.locateElement(locType, locValue))).build()
 				.perform();
+		test.log(LogStatus.INFO, "moved to element");
 		sleep(2000);
 	}
 
 //	moveToEleAndClick
 	public void moveToEleAndClick(String locType, String locValue, String testdata) {
 		Actions actions = new Actions(this.driver);
+		test.log(LogStatus.INFO, "locating an element using "+locType+" = "+locValue);
 		actions.moveToElement(this.driver.findElement(LocatorHelper.locateElement(locType, locValue))).click().build()
 				.perform();
+		test.log(LogStatus.INFO, "moved to element and clicked on it");
 		sleep(2000);
 	}
 
@@ -60,19 +75,23 @@ public class Keywords extends BrowserHelper {
 	public void switchToFrame(String locType, String locValue, String testdata) {
 		WebElement frameToSwitch = driver.findElement(LocatorHelper.locateElement(locType, locValue));
 		this.driver.switchTo().frame(frameToSwitch);
+		test.log(LogStatus.INFO, "switched to frame");
 	}
 
 //	select
 	public void select(String locType, String locValue, String testdata) {
+		test.log(LogStatus.INFO, "locating an element using "+locType+" = "+locValue);
 		WebElement ele = this.driver.findElement(LocatorHelper.locateElement(locType, locValue));
 		Select eleSelect = new Select(ele);
 		eleSelect.selectByVisibleText(testdata);
+		test.log(LogStatus.INFO, "selected option "+testdata);
 		sleep(2000);
 	}
 
 //	switchToDefaultContent
 	public void switchToDefaultContent(String locType, String locValue, String testdata) {
 		this.driver.switchTo().defaultContent();
+		test.log(LogStatus.INFO, "switched to default content i.e. main page");
 	}
 
 //	closeBrowser
@@ -82,7 +101,9 @@ public class Keywords extends BrowserHelper {
 
 //	clear
 	public void clear(String locType, String locValue, String testdata) {
+		test.log(LogStatus.INFO, "locating an element using "+locType+" = "+locValue);
 		this.driver.findElement(LocatorHelper.locateElement(locType, locValue)).clear();
+		test.log(LogStatus.INFO, "existing data in text filed clered");
 		sleep(2000);
 	}
 
@@ -94,6 +115,7 @@ public class Keywords extends BrowserHelper {
 		Set<String> windowHandles = this.driver.getWindowHandles();
 		List<String> windowIDs = new ArrayList<String>(windowHandles);
 		this.driver.switchTo().window(windowIDs.get(Integer.parseInt(testdata)));
+		test.log(LogStatus.INFO, "switched to window "+driver.getTitle());
 	}
 
 //	@Test
